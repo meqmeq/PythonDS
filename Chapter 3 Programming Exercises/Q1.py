d5f3865c988664689f36c8efbe3737c134840139
meqmeq <43584127+meqmeq@users.noreply.github.com>
2021-01-22 11:36:20 -0500

    Committing changes

--- Chapter 3 Programming Exercises/Q1.py
+++ Chapter 3 Programming Exercises/Q1.py
@@ -3,11 +3,19 @@ import random
 
 def get_index(l):
 
-
-
     for i in range(100000):
-        r = random.randint(0, len(l) - 1)
+        r = random.randint(0, len(l) - 2)
         l[r]
+
+def del_list(l):
+
+    for i in range(100):
+        if i >= len(l) - 10:
+            return
+        else:
+            r = random.randint(0,5)
+
+            del l[0]
     
 def get_time(f):
     start = timer()
@@ -17,8 +25,15 @@ def get_time(f):
 
 def get_item(d):
     
-    for i in range(100000):
+    for i in range(100):
         d[d.get(random.randint(0,100))] = 1
+
+def del_dict(d):
+    for i in range(100):
+        if i >= len(d):
+            return
+        else:
+            del d[i]
 
 def main():
 
@@ -40,5 +55,19 @@ def main():
         print(f"Dictionary length {len(d)}")
         print(f"Time to get() and set() in a dictionnary: {get_time(get_item(d))}")
 
-    #nani
+    print("List del() time")
+
+    for i in range(5):
+        l = [random.randint(0, 1000) for i in range(1000)]
+        print(f"List is lenght {len(l)}")
+        print(f"Time fo delete is {get_time(del_list(l))}")
+    ### Deleting Dictionnaries
+    for i in range(5):
+        # Let's initialise dictionaries
+        dict_len = range(1000)
+        keys = [random.randint(0,100) for i in range(len(dict_len))]
+        values = [random.randint(0,100) for i in range(len(dict_len))]
+        d = dict(zip(keys,values))
+        print(f"Dictionary length {len(d)}")
+        print(f"Time to del in a dictionnary: {get_time(del_dict(d))}")
 main()
