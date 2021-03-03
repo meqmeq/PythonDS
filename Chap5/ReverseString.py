a123dda9f11f34d43af39e90e5c9c57573b980bc
Marc <mecquilatan@gmail.com>
2021-03-03 14:11:04 -0500

    Adding Questions 1-5

--- Chap5/ReverseString.py
+++ Chap5/ReverseString.py
@@ -1,7 +1,10 @@
 def reverse(s):
-    if len(s) < 1:
-        return s
+    # if len(s) < 1:
+    #     return s
     if len(s) < 2:
         return s[len(s)-1]
     else:
         return s[len(s)-1] + reverse(s[:len(s)-1])
+
+
+print(reverse("hello"))
